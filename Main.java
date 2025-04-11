39acd28b166d0dbfc598924f377b234b56d78135
tttnamsss <165445210+tttnamsss@users.noreply.github.com>
2025-04-11 14:41:06 +0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,18 +1,27 @@
 public class ArraySorter {
 
   /**
-   * Sorts the given array in ascending order using bubble sort.
+   * Sorts the given array in ascending order using an optimized bubble sort.
    *
    * @param array the array to be sorted
    */
   public static void sortAscending(int[] array) {
+    if (array == null || array.length < 2) {
+      return; // Nothing to sort
+    }
+
     for (int i = 0; i < array.length - 1; i++) {
+      boolean swapped = false;
       for (int j = 0; j < array.length - i - 1; j++) {
         if (array[j] > array[j + 1]) {
           int temp = array[j];
           array[j] = array[j + 1];
           array[j + 1] = temp;
+          swapped = true;
         }
+      }
+      if (!swapped) {
+        break; // Array is already sorted
       }
     }
   }
@@ -27,3 +36,5 @@ public class ArraySorter {
     }
   }
 }
+
+
